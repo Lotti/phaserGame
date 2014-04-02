139d7ce3342838099e3945548007462fa5b1d52b
Lotti <lotti86@gmail.com>
2014-04-02 07:57:02 +0200

    and finally the rocks moves!

--- game/game.js
+++ game/game.js
@@ -1,5 +1,4 @@
 var debug = false;
-var gameDiv = "game";
 var mummy;
 var block;
 var background;
@@ -11,6 +10,7 @@ var jumpTimer = 0;
 var jumpPressed = false;
 var lastRandomLevel;
 
+var gameDiv = "game";
 var gameWidth = parseInt(document.getElementById(gameDiv).offsetWidth);
 var gameHeight = parseInt(document.getElementById(gameDiv).offsetHeight);
 
@@ -41,15 +41,14 @@ var PreloadState = {
 		game.load.image('halfBlock', 'res/sprites/halfBlock.gif');
 		game.load.image('block', 'res/sprites/block.gif');
 	},
-	create: function() {		
-		game.state.start('game');
+	create: function() {
+		game.state.start('game');		
 	}
 }
 
 
 var GameState = {
 	create: function() {
-		jumpPressed = false;
 		
 		game.physics.startSystem(Phaser.Physics.ARCADE);
 		game.physics.arcade.gravity.y = 250;
@@ -62,6 +61,13 @@ var GameState = {
 				
 		mummy = game.add.sprite(0, game.world.height - block.height*6, 'mummy');
 		mummy.name = 'mummy';
+		mummy.lifes = 3;
+		mummy.hit = 0;
+		
+		mummy.animationSpeed = 15;
+		mummy.walkSpeed = 5;
+		mummy.jumpSpeed = 150;
+		
     	game.physics.arcade.enable(mummy);
 		mummy.scale.setTo(1,1);
 		mummy.anchor.setTo(.5,.5);
@@ -69,6 +75,8 @@ var GameState = {
 		mummy.body.setSize(mummy.width*0.5,mummy.height,0,0);
 		mummy.body.collideWorldBounds = true;
 		mummy.animations.add('walk');
+		mummy.body.drag.set(1,1);
+		mummy.body.maxVelocity.set(30, 10000);
 		game.camera.follow(mummy);
 		
 		drawPlatform(game, block, platformData[0], game.world.height - block.height*6);
@@ -77,12 +85,19 @@ var GameState = {
 			game.time.advancedTiming = true;
 			fps = game.add.text(5, 2.5, '', { font: '20px Verdana', fill: '#FFFFFF', align: 'left' });
 			fps.fixedToCamera = true;
-			fps.update = function () {
+			fps.update = function() {
 				fps.setText(game.time.fps+' fps');
 			}
 		//}
 		
-		message = game.add.text(game.world.width*.3, 2.5, '', { font: '20px Verdana', fill: '#FFFFFF', align: 'left' });
+		message = game.add.text(game.world.width*.4, 2.5, '', { font: '20px Verdana', fill: '#FFFFFF', align: 'left' });
+		message.update = function() {
+			var t = 'lives: ';
+			for(var i = 0; i<mummy.lifes; i++) {
+				t+= '*';
+			}
+			message.setText(t);
+		}
 		message.fixedToCamera = true;
 		
 		points = game.add.text(game.world.width-5, 2.5, '0 points', { font: '20px Verdana', fill: '#FFFFFF', align: 'left' });
@@ -95,46 +110,95 @@ var GameState = {
 		}		
 	},
 	update: function() {
-		var animationSpeed = 15;
-		var speed = 5;
-		var maxSpeed = 30;
-		var jumpSpeed = 150;
-
 		if (gameover) {
-			if (game.input.activePointer.isDown) {
-				this.restart();
+			
+			message.setText("Game Over!");
+			
+			mummy.body.velocity.x = 0;
+			mummy.body.velocity.y = 0;
+			mummy.body.angularVelocity = 0;
+			mummy.body.allowGravity = false;
+			mummy.body.immovable = true;
+			mummy.animations.stop();
+		
+			rocks.forEach(function(e) {
+				e.body.velocity.x = 0;
+				e.body.velocity.y = 0;
+				e.body.angularVelocity = 0;
+				e.body.allowGravity = false;
+				e.body.immovable = true;	
+			});
+			
+			if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
+				this.restat();
 			}
 		}
 		else {
-			game.physics.arcade.collide(mummy, platforms, function(mummy, block) {
-			});
-			game.physics.arcade.collide(rocks, platforms, function(rock, block) {
-				block.life--;
-				if (block.life == 1) {
-					block.loadTexture("halfBlock",0);
+			if (mummy.lifes <= 0) {
+				gameover = true;
+			}
+			
+			//gem "no-physics" collision
+			if (gems != null) {
+				gems.forEach(function(e) {
+					if (e != undefined && Phaser.Rectangle.intersects(mummy.getBounds(), e.getBounds())) {
+						e.destroy();
+						points.p+=5;
+					}
+				});
+			}
+			
+			//mummy collisions
+			game.physics.arcade.collide(mummy, [platforms, rocks], function(mummy, o) {
+				if (o.name == "gem") {
+					points.p+=5;
+					o.destroy();
 				}
-				else if (block.life == 0) {
-					block.destroy();
+				else if (o.name == "rock") {
+					if (!mummy.hit) {
+						mummy.lifes--;
+						if (mummy.lifes > 0) {
+							mummy.hit = 1;
+							game.time.events.repeat(Phaser.Timer.SECOND * 0.15, 10, function() {
+								mummy.visible = !mummy.visible;
+								mummy.hit++;
+								if (mummy.hit > 10) {
+									mummy.hit = 0;
+									mummy.visible = true;
+								}
+							}, this);
+						}
+					}
 				}
 			});
-			game.physics.arcade.collide(mummy, rocks, function(mummy, rocks) {
+			
+			//rocks collisions
+			game.physics.arcade.collide(rocks, [gems,platforms,rocks], function(rock, o) {
+				if (o.name == "gem") {
+					o.destroy();
+				}
+				else if (o.name == "block") {
+					o.lifes--;
+					if (o.lifes == 1) {
+						o.loadTexture("halfBlock",0);
+					}
+					else if (o.lifes == 0) {
+						o.destroy();
+					}					
+				}
 			});
 							
 			if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-				mummy.animations.play("walk",animationSpeed);
+				mummy.animations.play("walk",mummy.animationSpeed);
 				mummy.scale.x = -Math.abs(mummy.scale.x);
-				mummy.body.velocity.x-= speed;
-				if (Math.abs(mummy.body.velocity.x) > maxSpeed) {
-					mummy.body.velocity.x = -maxSpeed;
-				}
+				
+				mummy.body.velocity.x-= mummy.walkSpeed;
 			}
 			else if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-				mummy.animations.play("walk",animationSpeed);
+				mummy.animations.play("walk",mummy.animationSpeed);
 				mummy.scale.x = Math.abs(mummy.scale.x);
-				mummy.body.velocity.x+= speed;
-				if (Math.abs(mummy.body.velocity.x) > maxSpeed) {
-					mummy.body.velocity.x = maxSpeed;
-				}
+				
+				mummy.body.velocity.x+= mummy.walkSpeed;
 			}
 			else {
 				mummy.body.velocity.x = 0;
@@ -144,7 +208,7 @@ var GameState = {
 			
 			if (!jumpPressed && game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR) && mummy.body.touching.down) {
 				jumpPressed = true;
-				mummy.body.velocity.y-= jumpSpeed;
+				mummy.body.velocity.y-= mummy.jumpSpeed;
 			}
 			
 			if (!game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR)) {
@@ -153,6 +217,8 @@ var GameState = {
 			
 			//load next platforms!
 			if (mummy.body.y+mummy.body.height > game.world.height - block.height*7) {
+				points.p++;
+				
 				var rnd;
 				do {
 					rnd = game.rnd.integerInRange(0,platformData.length-1);
@@ -169,6 +235,9 @@ var GameState = {
 	render: function() {
 		if (debug) {
 			game.debug.body(mummy);
+			gems.forEach(function(e) {
+				game.debug.body(e);
+			});
 			rocks.forEach(function(e) {
 				game.debug.body(e);
 			});
@@ -186,18 +255,17 @@ var GameState = {
 
 		//cleaning routines (remove upper platforms)
 		platforms.forEach(function (e) {
-			if (e != undefined && !e.inCamera && e.y+block.height < game.camera.y) {
+			if (e != undefined && !e.inCamera && e.y+block.height < game.camera.y*1.5) {
 				e.destroy();
 			}
 		});					
 	},	
 	restart: function() {
-		gameover = false;
+		gameover = false;		
+		jumpTimer = 0;
+		jumpPressed = false;
 		
-		game.state.start("game");
-		points.p = 0;
-
-		message.setText("");
+		game.state.start("game",true,false);
 	}	
 }
 
--- game/utils.js
+++ game/utils.js
@@ -1,4 +1,5 @@
-var platforms = null;
+var gems = game.add.group();
+var platforms = game.add.group();
 var rocks = null;
 var platformData = [
 	[
@@ -53,11 +54,11 @@ var platformData = [
 ];
 	
 function drawPlatform(game, block, platformData, y) {
-	if (platforms == null) {
-		platforms = game.add.group();
- 		//platforms.enableBody = true;
-    	//platforms.physicsBodyType = Phaser.Physics.ARCADE;		
-	}
+	if (!platforms.enableBody) {
+		platforms.enableBody = true;
+		platforms.physicsBodyType = Phaser.Physics.ARCADE;
+	}		
+		
 	var x = 0;
 	var y = (y !== undefined) ? y : game.world.height + block.height;
 	y+= block.height;
@@ -73,25 +74,35 @@ function drawPlatform(game, block, platformData, y) {
 				var jj = j;		
 			}		
 			if (platformData[i][jj]) {
-				var b = platforms.create(x+j*block.width, y+i*block.height, 'block', 12);
-				b.name = 'block';
-				b.life = 2;
-				//if (platformData[i][jj] != 2) {
-					game.physics.arcade.enable(b);
-					b.body.allowGravity = false;
-					b.body.immovable = true;		
-				//}
+				var b = platforms.create(x+j*block.width, y+i*block.height, "block");
+				b.name = "block";
+				b.lifes = 2;
+				b.body.allowGravity = false;
+				b.body.immovable = true;	
+		
+				if (platformData[i][jj] == 1 && (i-1 >= 0 && platformData[i-1][jj] == 0) && game.rnd.integerInRange(0,10) % 4 == 0) {
+					if (gems == null) {
+						gems = game.add.group();
+					}
+					var g = gems.create(x+j*block.width+block.width*0.5, y+i*block.height, "gem");
+					g.name = "gem";
+					g.alive = true;
+					g.anchor.setTo(0.5,1);
+					g.scale.setTo(0.5,0.5);
+					game.physics.arcade.enable(g);
+					g.body.allowGravity = false;
+					g.body.immovable = true;			
+				}
 			}
 		}
 	}
 }
 		
 function drawRocks(game, block) {
-	if (rocks == null) {
-		rocks = game.add.group();
- 		rocks.enableBody = true;
-    	rocks.physicsBodyType = Phaser.Physics.ARCADE;		
-	}		
+	if (!rocks.enableBody) {
+		rocks.enableBody = true;
+		rocks.physicsBodyType = Phaser.Physics.ARCADE;
+	}
 		
 	var lastP = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 	var p;
@@ -101,13 +112,17 @@ function drawRocks(game, block) {
 			p = game.rnd.integerInRange(0,game.world.width/block.width);
 		} while(lastP[p] == 1);
 		lastP[p] = 1;
-		r = rocks.create(p*block.width-18, game.camera.y - block.height, "rock", 11);
+		r = rocks.create(p*block.width-18, game.camera.y - block.height, "rock");
 		r.name = "rock";
+		r.lifes = 2;
 		r.anchor.setTo(0.5,0.5);
 		r.scale.setTo(1.5,1.5);
+		r.body.drag.setTo(0.5,0.5);
+		r.body.angularDrag = 0.5;
 		r.body.setSize(16,16,0,0);
 		r.body.bounce.setTo(0.2, 0.2);
 		r.body.angularVelocity = 250;
+		r.body.velocity.x = game.rnd.integerInRange(-150,150);
 		r.body.collideWorldBounds = true;
 	}
 }
